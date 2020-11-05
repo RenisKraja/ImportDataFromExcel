50d09ff83572ae661ce18e4145b5c7d3cabcda12
RenisKraja <RenisKraja@users.noreply.github.com>
2020-11-05 14:29:27 +0100

    Adding BackEnd Logic

---
+++ Models/Results.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImportDataFromExcel.Models
+{
+    public class Results
+    {
+        public string Status
+        {
+            get;
+            set;
+        }
+
+        public string Object
+        {
+            get;
+            set;
+        }
+
+        public string RecordCreated
+        {
+            get;
+            set;
+        }
+
+        public string RecordFailed
+        {
+            get;
+            set;
+        }
+
+        public string StartDate
+        {
+            get;
+            set;
+        }
+
+        public string ProcessingTime
+        {
+            get;
+            set;
+        }
+    }
+}
